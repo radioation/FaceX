c0b404729e03afa667d5c9af42471c9fc2fddac3
radioation <greggallardo@gmail.com>
2022-09-08 17:02:52 -0500

    Working on Python eyetracking data listener

--- ETDataListener/PyETDataListener/PyETDataListener.py
+++ ETDataListener/PyETDataListener/PyETDataListener.py
@@ -1,5 +1,6 @@
 
 import socket
+import sys
 
 GroupAddress = '234.5.6.7'
 EndPoint( "", 44511 ) 
@@ -20,4 +21,17 @@ while True:
 
     print('Read %s bytes' % (len(receiveBytes) ))
     fileTime = int.from_bytes( receiveBytes, "little" );
+    offset = 8
+    name = print( receiveBytes[offset:offset+32])
+    offset +=32
+    ObjectIntersectionName =  print( receiveBytes[offset:offset+64])
+    offset +=64
+    IntersectionIndex = int.from_bytes( receiveBytes[offset:offset+4], "little" )
+    offset += 4
+    ObjectIntersectionX = float.from_bytes( receiveBytes[offset:offset+4], "little" )
+    offset += 4
+    ObjectIntersectionY = float.from_bytes( receiveBytes[offset:offset+4], "little" )
+    offset += 4
+
+
      
