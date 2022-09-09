af298384292234725409319ac81b3ca621390bfb
radioation <greggallardo@gmail.com>
2022-09-09 11:18:32 -0500

    Python now reading data from MC group

--- ETDataListener/PyETDataListener/PyETDataListener.py
+++ ETDataListener/PyETDataListener/PyETDataListener.py
@@ -1,9 +1,10 @@
 
 import socket
 import sys
+import struct
 
 GroupAddress = '234.5.6.7'
-EndPoint( "", 44511 ) 
+EndPoint = ( "", 44511 ) 
 
 #Creates a UdpClient for reading incoming data.
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,21 +18,85 @@ sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 
 while True: 
-    receiveBytes = sock.recvfrom(4096)
+    receiveBytes = sock.recv(4096)
 
-    print('Read %s bytes' % (len(receiveBytes) ))
-    fileTime = int.from_bytes( receiveBytes, "little" );
+    fileTime = int.from_bytes( receiveBytes[0:8], "little" )
     offset = 8
-    name = print( receiveBytes[offset:offset+32])
+    name = receiveBytes[offset:offset+32].decode('utf-8')
     offset +=32
-    ObjectIntersectionName =  print( receiveBytes[offset:offset+64])
+    ObjectIntersectionName =  receiveBytes[offset:offset+64].decode('utf-8')
     offset +=64
-    IntersectionIndex = int.from_bytes( receiveBytes[offset:offset+4], "little" )
+    IntersectionIndex = int.from_bytes(receiveBytes[offset:offset + 4], "little")
     offset += 4
-    ObjectIntersectionX = float.from_bytes( receiveBytes[offset:offset+4], "little" )
+    ObjectIntersectionX, = struct.unpack('f',receiveBytes[offset:offset + 4])
     offset += 4
-    ObjectIntersectionY = float.from_bytes( receiveBytes[offset:offset+4], "little" )
+    ObjectIntersectionY, = struct.unpack('f',receiveBytes[offset:offset + 4])
     offset += 4
+
+    
+    HeadPosX, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    HeadPosY, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    HeadPosZ, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+
+
+    
+    LeftGazeOriginX, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    LeftGazeOriginY, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    LeftGazeOriginZ, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+
+        
+    LeftGazeDirectionX, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    LeftGazeDirectionY, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    LeftGazeDirectionZ, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+
+        
+    RightGazeOriginX, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    RightGazeOriginY, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    RightGazeOriginZ, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+
+        
+    RightGazeDirectionX, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    RightGazeDirectionY, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    RightGazeDirectionZ, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+
+    
+    LeftGazeType = int.from_bytes(receiveBytes[offset:offset + 4], "little")
+    offset += 4
+    RightGazeType = int.from_bytes(receiveBytes[offset:offset + 4], "little")
+    offset += 4
+        
+    HeadX, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    HeadY, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    HeadZ, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+
+        
+    HeadDirectionX, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    HeadDirectionY, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+    HeadDirectionZ, = struct.unpack('f',receiveBytes[offset:offset + 4])
+    offset += 4
+     
+
+    print(fileTime, name, ObjectIntersectionName,IntersectionIndex,ObjectIntersectionX,ObjectIntersectionY,HeadPosX,HeadPosY,HeadPosZ,LeftGazeOriginX,LeftGazeOriginY,LeftGazeOriginZ,LeftGazeDirectionX,LeftGazeDirectionY,LeftGazeDirectionZ,RightGazeOriginX,RightGazeOriginY,RightGazeOriginZ,RightGazeDirectionX,RightGazeDirectionY,RightGazeDirectionZ,LeftGazeType,RightGazeType,HeadX,HeadY,HeadZ,HeadDirectionX,HeadDirectionY,HeadDirectionZ); 
 
 
      
